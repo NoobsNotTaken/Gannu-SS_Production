35e69108e13f117dbaa757223ba2a89309dc989c
Mantri Venkata Sai Nischal <sainischalmv@gmail.com>
2024-08-28 23:01:47 +0530

    title componet created

---
+++ src/Title.jsx
@@ -0,0 +1,28 @@
+function Title() {
+  return (
+    <>
+      <div className="flex flex-col gap-4">
+        {" "}
+        <div className="text-3xl font-semibold ">
+          {" "}
+          Innvoative UI/UX Designer
+        </div>
+        <div className="text-xl w-full font-medium text-pretty ">
+          <div className="">
+            Crafting Exceptional Degital Experience With a User-Centered
+            Approach
+          </div>
+        </div>
+        <div className=" flex text-right mt-10 text-sm font-normal">
+          <div className="flex-1"></div>
+          <div className="justify-end w-[40%] text-pretty">
+            We combine creativity with a deep understanding of user behavior to
+            build digital experiences that engage and inspire.
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Title;
