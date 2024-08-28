2ba0f0c3724e5c07a566bf810802d5a3e164bdfe
Mantri Venkata Sai Nischal <sainischalmv@gmail.com>
2024-08-28 23:02:17 +0530

    updated Home

--- src/home.jsx
+++ src/home.jsx
@@ -1,5 +1,27 @@
+import Content from "./Content";
+import Footer from "./Footer";
+import Title from "./Title";
+import Header from "./Header";
+
 function Home() {
-  return <div className="text-xl font-bold "> Hello World !</div>;
+  return (
+    <>
+      <div className="h-screen w-full px-16 py-6 ">
+        <div className="">
+          <Header />
+        </div>
+        <div className="flex justify-start flex-col mt-10 ">
+          <Title />
+        </div>
+        <div className="flex justify-center items-center">
+          <Content />
+        </div>
+      </div>
+      <div className="flex justify-center items-center">
+        <Footer />
+      </div>
+    </>
+  );
 }
 
 export default Home;
